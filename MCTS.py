bcb7e8dc22295707ea68139f271986e6b0f97575
chrisdybdahl <chrisdybdahl@gmail.com>
2024-04-17 13:53:07 +0200

    Initial implementation of a tree for MCTS

--- MCTS.py
+++ MCTS.py
@@ -1,11 +1,55 @@
+class Node:
+    def __init__(self, parent=None, game: object = None):
+        """
+        Initializes the node
+
+        :param parent: Parent node
+        """
+        self.parent = parent
+        self.game = game
+        self.children = []
+        self.visits = 0
+        self.value = 0  # Action value?
+
+    def expand(self):
+        """
+        Expands the current node
+
+        """
+        return ValueError
+
+    def parent(self):
+        """
+        Returns the current node's parent
+
+        :return: parent node
+        """
+        return self.parent
+
+    def update_value(self, new_value):
+        """
+        Updates the current node's value'
+
+        :param new_value: new value to update
+        """
+        self.value = new_value
+
+    def get_value(self):
+        """
+        Returns the current node's value'
+
+        :return: float representing current node's value'
+        """
+        return self.value
+
+
 class MCTS:
     def __init__(self, M):
         """
         Initialize MCTS parameters here
 
-        Parameters:
-            M (int): the number simulations each move in the actual game stems from,
-            often 500 suffices (can be dynamic / time limited)
+        :param M (int) the number simulations each move in the actual game stems
+        from, often 500 suffices (can be dynamic / time limited):
         """
         self.M = M
 
@@ -13,42 +57,43 @@ class MCTS:
         """
         Traverse the tree from the root to a leaf node using tree policy
 
-
         """
         self.root = 1
 
-    def expand_node(self, node):
+    def expand_node(self, node: Node):
         """
-
-        Parameters:
-            node: node to expand
-
         Generate some or all child states of a parent node , and connect the tree node housing the parent state
         (aka. parent node) to the nodes housing the child states (aka. child nodes)
 
+        :param node: node to expand
         """
         self.root = 1
 
-    def evaluate_leaf(self, node):
+    def evaluate_leaf(self, node: Node) -> float:
         """
-        Estimate the value of the leaf node in the tree by doing a rollout simulation using the default olicy from
+        Estimate the value of the leaf node in the tree by doing a rollout simulation using the default policy from
         the leaf node's state to a final state.
         It the vale of the node should be evaluated using an entire rollout game
 
-        Parameters:
-            node (Node): the node to evaluate
-
+        :return: value of the leaf node
+        :param node: the node to evaluate
         """
         self.node = node
-        return node.value
+        return node.get_value()
 
     def backpropagation(self, node):
         """
         Passing the evaluation of a final state back up the tree, updating the relevant data at all nodes and edges
         on the path from the final state to the tree root
 
-        Parameters:
-            node (Node): the node to backpropagate from
-
+        :param node: the node to backpropagate from
         """
         self.node = node
+
+    def run(self):
+        """
+        Run the MCTS
+
+        :return: best action
+        """
+        return ValueError
--- NIM.py
+++ NIM.py
@@ -3,19 +3,34 @@ class NIM:
         """
         Creates an instance of the NIM board and initializes parameters
 
-        Parameters:
-            N (int): number of pieces on the board
-            K (int): number of pieces a player maximum can take off the board
-        N is the number of pieces on the board
-
+        :param N: number of pieces on the board
+        :param K: number of pieces a player maximum can take off the board
         """
         self.N = N
         self.K = K
+        self.current_player = 1
 
-    def remove(self, removed_pieces):
+    def do_action(self, removed_pieces):
         """
         Removes pieces from the board
 
-        Parameters:
-            removed_pieces: list of touples"""
+        :param removed_pieces: list of tuples which represent the coordinates of the pieces to remove
+        """
         self.N = removed_pieces
+        self.current_player = 3 - self.current_player
+
+    def get_state(self):
+        """
+        Returns the state of the game
+
+        :return: 0 no win yet, 1 player_1 win, 2 player_1 win
+        """
+        return ValueError
+
+    def current_player(self):
+        """
+        Returns the current player of the game
+
+        :return: integer representing the current player
+        """
+        return ValueError
